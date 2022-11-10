e31b96040470d4f94b66be68266154a6ff3b4853
Tim T <tim.email.personal@gmail.com>
2022-11-09 18:07:20 -0800

    Add fetch functions in apiCalls

---
+++ src/apiCalls.js
@@ -0,0 +1,48 @@
+
+const urlCustomers = 'http://localhost:3001/api/v1/customers'
+const urlRooms = 'http://localhost:3001/api/v1/rooms'
+const urlBookings = 'http://localhost:3001/api/v1/bookings'
+
+function getData(url) {
+  return fetch(url)
+    // .then(response => response.json())
+    .then((response) => {
+      if (response.ok) {
+        return response.json()
+      } else {
+        throw Error(response.statusText)
+      }
+    })
+    .catch(err => {
+      // unhide element here showing error message 
+      console.log(err)});
+}
+
+const getAllData = () => {
+  return Promise.all([
+    getData(urlBookings),
+    getData(urlRooms),
+    getData(urlCustomers),
+  ])
+}
+
+function postData(body, url) {
+  return fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+    .then((response) => {
+      if (response.ok) {
+        return response.json()
+      } else {
+        throw Error(response.statusText)
+      }
+    })
+    .catch(err => console.log(err))
+    // unhide element here showing error message 
+  })
+}
+
+export { getData, postData, getAllData } 
