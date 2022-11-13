8bb480d43f92ca2c53abd01ea0d846a72e2e2395
Tim T <tim.email.personal@gmail.com>
2022-11-13 14:33:04 -0800

    Update Customer tests with happy and sad paths, pass all tests

--- test/Bookings-test.js
+++ test/Bookings-test.js
@@ -1,14 +1,13 @@
 import chai from 'chai'
 const expect = chai.expect
-import { Bookings } from '../src/classes/Bookings'
+import { Bookings, Room } from '../src/classes/Bookings'
 import {testDataCustomer, testDataRooms, testDataBookings} from '../src/data/test-data'
 
 describe('Bookings', () => {
   let bookings 
-  // console.log("LOOK HERE +++", testDataBookings.bookings[0])
 
   beforeEach(() => {
-    bookings = new Bookings(testDataBookings.bookings[0])
+    bookings = new Bookings(testDataBookings, testDataRooms)
   })
 
   it('should be an instance of Bookings', () => {
@@ -16,11 +15,11 @@ describe('Bookings', () => {
   })
 
   it('should have an ID', () => {
-    expect(bookings.bookingsId).to.equal("5fwrgu4i7k55hl6sz")
+    expect(bookings[0].bookingsId).to.equal("5fwrgu4i7k55hl6sz")
   })
 
   it('should have a user Id', () => {
-    expect(bookings.bookingsUserId).to.equal(1)
+    expect(bookings[0].bookingsUserId).to.equal(1)
   })
 
   it('should have a date', () => {
--- test/Customer-test.js
+++ test/Customer-test.js
@@ -22,4 +22,35 @@ describe('Customer', () => {
     expect(customer.customerId).to.equal(1)
   })
 
+  it('should get customer index', () => {
+    let customerData = testDataCustomer.customers
+    const index = customer.getCustomerIndex(customerData, 2)
+    expect(index).to.equal(1)
+    const index2 = customer.getCustomerIndex(customerData, 30)
+    expect(index2).to.equal(-1)
+  })
+
+  it('should test if log in credentials are verified', () => {
+    let customerData = testDataCustomer.customers
+    let password = "overlook2021"
+    let username = 2
+    const verified = customer.checkCredentials(username, password, customerData)
+    expect(verified).to.equal(true)
+
+  })
+  it('should test if log in password is not verified', () => {
+    let customerData = testDataCustomer.customers
+    let password = "Overlook2021"
+    let username = 2
+    const verified = customer.checkCredentials(username, password, customerData)
+    expect(verified).to.equal(false)
+  })
+
+  it('should test if log in username is not verified', () => {
+    let customerData = testDataCustomer.customers
+    let password = "overlook2021"
+    let username = 600
+    const verified = customer.checkCredentials(username, password, customerData)
+    expect(verified).to.equal(false)
+  })
 })
